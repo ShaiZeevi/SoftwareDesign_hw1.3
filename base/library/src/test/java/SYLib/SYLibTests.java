dd8a5ff7f0a9d6577acc9fe223406a8d4b4f1a13
yoavz <yoavzuriel@cs.technion.ac.il>
2018-05-13 18:38:50 +0300

    some sylib tests

--- base/library/src/test/java/SYLib/SYLibTests.java
+++ base/library/src/test/java/SYLib/SYLibTests.java
@@ -1,20 +1,15 @@
 package SYLib;
 
-import SYlib.CollectionAlreadyExistsException;
-import SYlib.CollectionDoesNotExistException;
-import SYlib.ISYLibable;
-import SYlib.StorageAlreadyExistsException;
-import com.google.inject.Guice;
-import com.google.inject.Injector;
-import il.ac.technion.cs.sd.pay.ext.SecureDatabaseModule;
+import SYlib.*;
+import il.ac.technion.cs.sd.pay.ext.SecureDatabase;
+import org.mockito.Mockito;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.zip.DataFormatException;
 
 import static org.junit.Assert.fail;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
@@ -23,8 +18,8 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class SYLibTests {
     @Test
     public void cantCreateSameStorageTwiceTest() {
-        Injector injector = Guice.createInjector(new SecureDatabaseModule(), new SYLibTestModule());
-        ISYLibable myLib = injector.getInstance(ISYLibable.class);
+        SecureDatabase mockDB = Mockito.mock(SecureDatabase.class);
+        ISYLibable myLib = new SYLib((unused) -> mockDB);
         try {
             myLib.openStorage("Storage1");
         } catch (StorageAlreadyExistsException e) {
@@ -35,39 +30,50 @@ public class SYLibTests {
 
     @Test
     public void cantSaveTheSameCollectionTwiceTest() {
-        Injector injector = Guice.createInjector(new SecureDatabaseModule(), new SYLibTestModule());
-        ISYLibable myLib = injector.getInstance(ISYLibable.class);
+        SecureDatabase mockDB = Mockito.mock(SecureDatabase.class);
         try {
-            myLib.saveCollection("collection1", new ArrayList<>());
+            Mockito.when(mockDB.get("Collection1".getBytes())).thenReturn(new byte[]{0}, new byte[]{1});
+        } catch (InterruptedException | DataFormatException e) {
+            fail();
+        }
+        ISYLibable myLib = new SYLib((unused) -> mockDB);
+        try {
+            myLib.saveCollection("Collection1", new ArrayList<>());
         } catch (CollectionAlreadyExistsException e) {
             fail();
         }
-        assertThrows(CollectionAlreadyExistsException.class, () -> myLib.saveCollection("collection1", new ArrayList<>()));
+        assertThrows(CollectionAlreadyExistsException.class, () -> myLib.saveCollection("Collection1", new ArrayList<>()));
     }
 
-    @Test
-    public void cantRestoreUnsavedCollectionTest(){
-        Injector injector = Guice.createInjector(new SecureDatabaseModule(), new SYLibTestModule());
-        ISYLibable myLib = injector.getInstance(ISYLibable.class);
-        assertThrows(CollectionDoesNotExistException.class, () -> myLib.restoreCollection("Collection1"));
-    }
-
-    @Test
-    public void restoreCollectionTest() {
-        Injector injector = Guice.createInjector(new SecureDatabaseModule(), new SYLibTestModule());
-        ISYLibable myLib = injector.getInstance(ISYLibable.class);
-        ArrayList<String> arrayList = new ArrayList<>();
-        for (Integer i = 10; i < 100; i++) {
-            arrayList.add(i.toString());
-        }
-        try {
-            myLib.saveCollection("arrayList", arrayList);
-            Collection<String> collection = myLib.restoreCollection("arrayList");
-            Iterator targetIt = collection.iterator();
-            for (Object obj : arrayList)
-                assertEquals(obj, targetIt.next());
-        } catch (CollectionAlreadyExistsException | CollectionDoesNotExistException e) {
-            fail();
-        }
-    }
+//
+//    @Test
+//    public void cantRestoreUnsavedCollectionTest(){
+//        SecureDatabase mockDB = Mockito.mock(SecureDatabase.class);
+//        try {
+//            Mockito.when(mockDB.get("Collection1".getBytes())).thenThrow(new NoSuchElementException());
+//        } catch (InterruptedException | DataFormatException e) {
+//            fail();
+//        }
+//        ISYLibable myLib = new SYLib((unused) -> mockDB);
+//        assertThrows(CollectionDoesNotExistException.class, () -> myLib.restoreCollection("Collection1"));
+//    }
+//
+//    @Test
+//    public void restoreCollectionTest() {
+//        SecureDatabase mockDB = Mockito.mock(SecureDatabase.class);
+//        ISYLibable myLib = new SYLib((unused) -> mockDB);
+//        ArrayList<String> arrayList = new ArrayList<>();
+//        for (Integer i = 10; i < 100; i++) {
+//            arrayList.add(i.toString());
+//        }
+//        try {
+//            myLib.saveCollection("arrayList", arrayList);
+//            Collection<String> collection = myLib.restoreCollection("arrayList");
+//            Iterator targetIt = collection.iterator();
+//            for (Object obj : arrayList)
+//                assertEquals(obj, targetIt.next());
+//        } catch (CollectionAlreadyExistsException | CollectionDoesNotExistException e) {
+//            fail();
+//        }
+//    }
 }
